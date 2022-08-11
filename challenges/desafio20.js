e44255b74f0ff8c7bbcab603e6b20c6efe6dfc7f
allpha23 <allpha23@gmail.com>
2022-08-10 22:39:28 -0300

    feat: desafio20

---
+++ challenges/desafio20.js
@@ -0,0 +1,9 @@
+db.produtos.updateOne(
+{ nome: { $eq: "Quarteirão com Queijo" } },
+{ $pop: { ingredientes: -1 } },
+);
+
+db.produtos.find(
+{},
+{ nome: true, ingredientes: true, _id: false },
+);
